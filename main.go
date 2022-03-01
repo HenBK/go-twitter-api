de67249532e28e06799161780f2bf02913ef6c2d
Henrique Kubenda <henrique.kubenda@cornershopapp.com>
2022-03-01 18:58:18 -0300

    updated main.go file to test database connection

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/henbk/go-twitter-api/db"
+	"github.com/henbk/go-twitter-api/handlers"
+)
+
+func main() {
+	fmt.Println(db.GetConnectionStatus())
+	handlers.Handlers()
 }
