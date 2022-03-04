38feda6aa8dd137d6333933e7782bbfb60f1f3d5
Henrique Kubenda <henrique.kubenda@cornershopapp.com>
2022-03-04 19:34:35 -0300

    moved database connection configuration variables inside establishDatabaseConnection function

--- db/databaseConnection.go
+++ db/databaseConnection.go
@@ -1,7 +1,7 @@
 package db
 
 import (
-	ctx "context"
+	"context"
 	"fmt"
 	"log"
 
@@ -11,20 +11,20 @@ import (
 
 var MongoConnection *mongo.Client = establishDatabaseConnection()
 
-var serverAPIOptions *options.ServerAPIOptions = options.ServerAPI(options.ServerAPIVersion1)
-
-var clientOptions *options.ClientOptions = options.Client().
-	ApplyURI("mongodb+srv://admindb:zjNTUYnm41cDYTSC@cluster0.jx2gd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority").
-	SetServerAPIOptions(serverAPIOptions)
-
 func establishDatabaseConnection() *mongo.Client {
-	client, err := mongo.Connect(ctx.TODO(), clientOptions)
+	var serverAPIOptions *options.ServerAPIOptions = options.ServerAPI(options.ServerAPIVersion1)
+
+	var clientOptions *options.ClientOptions = options.Client().
+		ApplyURI("mongodb+srv://admindb:zjNTUYnm41cDYTSC@cluster0.jx2gd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority").
+		SetServerAPIOptions(serverAPIOptions)
+
+	client, err := mongo.Connect(context.TODO(), clientOptions)
 
 	if err != nil {
 		log.Fatalf("Error while trying to connect to MongoDB, error: %v", err)
 	}
 
-	err = client.Ping(ctx.TODO(), nil)
+	err = client.Ping(context.TODO(), nil)
 
 	if err != nil {
 		log.Fatalf("Error while trying to ping to the database server, error: %v", err)
@@ -36,7 +36,14 @@ func establishDatabaseConnection() *mongo.Client {
 }
 
 func GetConnectionStatus() bool {
-	err := MongoConnection.Ping(ctx.TODO(), nil)
-
-	return err != nil
+	err := MongoConnection.Ping(context.TODO(), nil)
+	return err == nil
 }
+
+// func CheckUserExists() bool {
+// 	return false
+// }
+
+// func InsertRow(t int) (int, bool) {
+
+// }
