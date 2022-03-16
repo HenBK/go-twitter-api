ed0cdae6e5b3c94d4283f60e8245406d839111c0
Henrique Kubenda <henrique.kubenda@cornershopapp.com>
2022-03-16 15:16:45 -0300

    updated handler function of /register endpoint to use db functions to interact with MongoDB instance

--- routers/register.go
+++ routers/register.go
@@ -5,13 +5,14 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/henbk/go-twitter-api/db"
 	"github.com/henbk/go-twitter-api/models"
 )
 
 func Register(rw http.ResponseWriter, r *http.Request) {
-	var t models.User
+	var user models.User
 
-	err := json.NewDecoder(r.Body).Decode(&t)
+	err := json.NewDecoder(r.Body).Decode(&user)
 
 	if err != nil {
 		fmt.Printf("Json object was not decoded succesfully %v", err)
@@ -21,34 +22,36 @@ func Register(rw http.ResponseWriter, r *http.Request) {
 
 	// Validations
 
-	if len(t.Email) == 0 {
+	if len(user.Email) == 0 {
 		http.Error(rw, "User email is required", 400)
 		return
 	}
 
-	if len(t.Password) < 6 {
+	if len(user.Password) < 6 {
 		http.Error(rw, "The user password must be at least 6 characters long", 400)
 		return
 	}
 
-	// if db.CheckUserExists() {
-	// 	http.Error(rw, "There is an already created user with this emai", 400)
-	// 	return
-	// }
+	_, userExists, _ := db.CheckUserExists(user.Email)
 
-	// // Registry step
+	if userExists {
+		http.Error(rw, "There is an already created user with this emai", 400)
+		return
+	}
 
-	// _, status, err := db.RegisterUser(t)
+	// Registration
 
-	// if err != nil {
-	// 	http.Error(rw, "An error ocurred while inserting the new user into the database. "+err.Error(), 500)
-	// 	return
-	// }
+	_, status, err := db.InsertUser(user)
 
-	// if !status {
-	// 	http.Error(rw, "An error ocurred while inserting the new user into the database.", 500)
-	// 	return
-	// }
+	if err != nil {
+		http.Error(rw, "An error ocurred while inserting the new user into the database. "+err.Error(), 500)
+		return
+	}
+
+	if !status {
+		http.Error(rw, "An error ocurred while inserting the new user into the database.", 500)
+		return
+	}
 
 	rw.WriteHeader(http.StatusCreated)
 
