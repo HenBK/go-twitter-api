87b8a444ad65b12da31a09d5ff5c49531ebd6f30
Henrique Kubenda <henrique.kubenda@cornershopapp.com>
2022-05-08 18:29:28 -0400

    added method to generate a json web token for a given user

---
+++ jwt/jwt.go
@@ -0,0 +1,36 @@
+package jwt
+
+import (
+	"os"
+	"time"
+
+	jwt "github.com/golang-jwt/jwt"
+	"github.com/henbk/go-twitter-api/models"
+)
+
+func GenerateJsonWebToken(user models.User) (string, error) {
+	SignPass := os.Getenv("JWT_SECRET_PASS")
+
+	claims := jwt.MapClaims{
+		"email":            user.Email,
+		"name":             user.Name,
+		"lastname":         user.Lastname,
+		"birthDate":        user.BirthDate,
+		"bio":              user.Bio,
+		"location":         user.Location,
+		"website":          user.Website,
+		"_id":              user.ID.Hex(),
+		"token_expiration": time.Now().Add(time.Hour * 24).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	tokenString, err := token.SignedString([]byte(SignPass))
+
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+
+}
